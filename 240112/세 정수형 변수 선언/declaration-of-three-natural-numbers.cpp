9d3184e5cb07a1dc0a5df5c1b48f2313f6d1afab
Baek Gyeong Min <80105027+SSODADA@users.noreply.github.com>
2024-01-12 16:46:07 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    a=7;
+    b=23;
+    c=30;
+    cout << a << " + " << b << " = " << c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
