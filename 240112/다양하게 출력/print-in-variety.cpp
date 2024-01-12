321d0dd3cd7b8d6983b1f8a2a4a0f39ee552f933
Baek Gyeong Min <80105027+SSODADA@users.noreply.github.com>
2024-01-12 15:33:45 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Total days in Year"; cout << endl;
+    cout << "365"; cout << endl;
+    cout <<"Circumference rate"; cout << endl;
+    cout << "3.1415926535";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
