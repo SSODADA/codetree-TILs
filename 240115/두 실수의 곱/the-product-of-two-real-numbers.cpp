33e4bfad99193e3c07f03ca433c99eb29b9b9fdb
Baek Gyeong Min <80105027+SSODADA@users.noreply.github.com>
2024-01-15 16:01:33 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 5.26;
+    double b = 8.27;
+
+    cout << fixed;;
+    cout.precision(3);
+    cout << a*b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
