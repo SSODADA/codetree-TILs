081ccf4ec200ed77d62f53d70cb033df2107b8bc
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-18 14:01:56 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/1시간 뒤 시간 출력/print-one-hour-later.cpp
+++ 240118/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int h;
-    double m;
-
-    cin >> h >> m;
-    cout << h+1 << ":" << m+59;
+    int h, m;
+    cin >> h;
+    cin.get();
+    cin >> m;
+    cout << h+1 << ":" << m;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
