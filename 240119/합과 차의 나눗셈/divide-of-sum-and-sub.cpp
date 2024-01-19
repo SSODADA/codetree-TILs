dae3d7516846ad1aa1951f2690079837e4680fc1
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-19 13:38:41 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b, c, d;
+    cin >> a >> b;
+    c = a + b;
+    d = a - b;
+    cout << fixed;
+    cout.precision(2);
+    cout << c/d;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
