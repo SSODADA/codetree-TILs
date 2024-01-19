3ab3f17933f2418d2ca60616b02e48849db2001b
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-19 14:04:46 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240119/합과 평균/sum-and-avg.cpp
+++ 240119/합과 평균/sum-and-avg.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     double a, b;
     cin >> a >> b;
-    cout << a+b << " " << (a+b)/2;
+    cout << a+b << " " << (double)(a+b)/2;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
