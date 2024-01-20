786fac89d5e19e89b6b72672d26cd73c724d1667
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-20 21:14:28 +0900

    Title: 정사각형의 넓이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a;
+    b = a*a;
+    cout << b << endl;
+    if (a < 5) {
+        cout << "tiny";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
