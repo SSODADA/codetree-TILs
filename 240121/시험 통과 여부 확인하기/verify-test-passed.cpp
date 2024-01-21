658ddfaacece83c48cfdd2bc422c0ed826235354
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-21 22:48:44 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+
+    if (a >= 80) {
+        cout << "pass";
+    }
+    else {
+        cout << a << "more score";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
