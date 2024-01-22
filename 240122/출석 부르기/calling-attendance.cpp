8b9107b5b63ff062a6495e3b3ce591d3ddf3b475
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-22 15:55:13 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/출석 부르기/calling-attendance.cpp
+++ 240122/출석 부르기/calling-attendance.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    if (a = 1) {
+    if (a == 1) {
         cout << "John";
     }
-    else if (a = 2) {
+    else if (a == 2) {
         cout << "Tom";
     }
-    else if (a = 3) {
+    else if (a == 3) {
         cout << "Paul";
     }
     else {
