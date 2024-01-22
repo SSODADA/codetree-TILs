a26566c2af86caec9471a85715bca2c5a01e62f3
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-22 14:31:24 +0900

    Title: 삼항연산자 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a = 1) {
+        cout << "t";
+    }
+    else {
+        cout << "f";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
