65ba91b3c5026a64e2617cd9e35cdecfc3c4b23e
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-22 14:30:24 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a >= 100) {
+        cout << "pass";
+    }
+    else {
+        cout << "failure";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
