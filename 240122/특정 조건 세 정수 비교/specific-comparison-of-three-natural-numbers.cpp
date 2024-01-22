fd45e5bd0530c173819031ed0d789d5c4fda1150
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-22 17:19:52 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240122/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 240122/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -4,10 +4,10 @@ int main() {
     int a, b, c;
     cin >> a >> b >> c;
     if (b > c) {
-        cout << (a==c) << " ";
+        cout << (a == c) << " ";
     }
-    else if (b < c) {
-        cout << (a==b) << " ";
+    else {
+        cout << (a == b) << " ";
     }
 
     cout << (a == b && a == c);
