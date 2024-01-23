a5399f82648bacee3059ce87760d1eb3e9bb6504
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-23 14:15:22 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240123/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240123/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a<b && a<c) {
+    if (a<=b && a<=c) {
         cout << a;
     }
-     if (b<a && b<c) {
+     if (b<=a && b<=c) {
         cout << b;
     }
-     if (c<a && c<b) {
+     if (c<=a && c<=b) {
         cout << c;
     }
     // 여기에 코드를 작성해주세요.
