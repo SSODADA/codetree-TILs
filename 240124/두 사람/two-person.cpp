d947f5e59b5e1dccb864b75fc9e3f0564bf35164
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-24 14:24:39 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/두 사람/two-person.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a_age, b_age;
+    char a_gender, b_gender;
+    cin >> a_age >> a_gender >> b_age >> b_gender;
+    
+    if((a_age >= 19 && a_gender == 'M') || (b_age >= 19 && b_gender == 'M')) {
+        cout << 1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
