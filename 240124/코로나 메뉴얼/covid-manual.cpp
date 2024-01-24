6136dd7ae970b3ba5bc824b4b816ff0c8078b078
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-24 16:44:26 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char a_1, b_1, c_1;
+    int a_2, b_2, c_2;
+    cin >> a_1;
+    cin.get();
+    cin >> a_2;
+    cin.get();
+    cin >> b_1;
+    cin.get();
+    cin >> b_2;
+    cin.get();
+    cin >> c_1;
+    cin.get();
+    cin >> c_2;
+    int n;
+    n = 0;
+    
+    if (a_1 = 'Y' && a_2 >= 37) {
+        n += 1;
+    }
+    if (b_1 = 'Y' && b_2 >= 37) {
+        n += 1;
+    }
+    if (c_1 = 'Y' && c_2 >= 37) {
+        n += 1;
+    }
+    if (n >= 2) {
+        cout << "E";
+    }
+    else cout << "N";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
