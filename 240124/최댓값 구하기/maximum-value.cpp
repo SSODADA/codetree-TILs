489e79555d7876de010aeedf4a587d4a801df1f0
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-24 15:22:39 +0900

    Title: 최댓값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/최댓값 구하기/maximum-value.cpp
+++ 240124/최댓값 구하기/maximum-value.cpp
@@ -6,13 +6,30 @@ int main() {
     if (a>b && a>c) {
         cout << a;
     }
-    if (b>a && b>c) {
+    else if (b>a && b>c) {
         cout << b;
     }
-    if (c>a && c>b) {
+    else if (c>a && c>b) {
         cout << c;
     }
-    
+    else if (a>=b && a>c) {
+        cout << a;
+    }
+    else if (a>b && a>=c) {
+        cout << a;
+    }
+    else if (b>=a && b>c) {
+        cout << b;
+    }
+    else if (b>a && b>=c) {
+        cout << b;
+    }
+    else if (c>=a && c>b) {
+        cout << c;
+    }
+    else if (c>a && c>=b) {
+        cout << c;
+    }
     // 여기에 코드를 작성해주세요.
     return 0;
 }
