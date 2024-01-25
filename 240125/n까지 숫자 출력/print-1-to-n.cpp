ceb490c4b94f26f8d408a849edf848e06c315acd
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-25 16:42:56 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int i;
+    i = 1;
+    while (i<=n) {
+        cout << i << " ";
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
