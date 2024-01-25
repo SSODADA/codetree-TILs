6926b839a1741cba1d65563e146756398e471c5c
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-25 15:40:44 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240125/홀수만 출력/output-only-odd.cpp
+++ 240125/홀수만 출력/output-only-odd.cpp
@@ -5,10 +5,8 @@ int main() {
     cin >> a >> b;
     for (int i = 1; (i>=a && i<=b); i++) {
         if (i%2 != 0) {
-            cout << i << " ";
+                cout << i << " ";
+            }
         }
-        else continue;
-    }
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
