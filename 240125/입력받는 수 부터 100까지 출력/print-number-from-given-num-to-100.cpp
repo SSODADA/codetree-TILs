d04f62c6b30b143ab619fd47a6742dc77ad61a4b
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-25 13:38:01 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240125/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if (n < 101){ 
+        cout << n << " ";
+    for (int n = n; n < 101; n++;) {
+        cout << n << " ";
+    }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
