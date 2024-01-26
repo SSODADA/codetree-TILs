8f7f46ac3292a378d1df18cb16e18fdb6add52bb
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-26 15:20:48 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int i = a;
+    while (i <= b) {
+        if (i %2 == 0) {
+        cout << i << " ";
+        i+=2;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
