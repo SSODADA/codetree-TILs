5145141c7008cfa80e36757833073c9025a0eea8
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-29 16:27:32 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 240129/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+
 int main() {
     int n;
-    int temp;
-    cin >> n;
     
-    for(int i = 1; i<=6; i++) {
-        temp = n;
-        cin >> n;
-        
-        if (temp!=2 && temp%3==0) {
-            cout << temp << endl;
-        }
-       
+    std::cin >> n;
+
+    std::vector<int> numbers(n);
+    
+    for (int i = 0; i < n; i++) {
+        std::cin >> numbers[i];
     }
+
+
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] % 2 == 1 && numbers[i] % 3 == 0) {
+            std::cout << numbers[i] << std::endl;
+        }
+    }
+
     return 0;
 }
