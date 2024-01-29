b03a39a1707906fbf0cfdfb013690e23f47f5f28
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-29 14:12:08 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240129/369 게임/369-game.cpp
+++ 240129/369 게임/369-game.cpp
@@ -6,14 +6,14 @@ int main() {
     for(int i = 1; i<=n; i++) {
         if(i>10) {
             if(i%10==3 || i%10==6 || i%10==9) {
-                cout << 0;
+                cout << 0 << " ";
             }
             else cout << i << " ";
         }
         
         if (i<10) {
             if(i%3==0 || i%6==0 || i%9==0) {
-                cout << 0;
+                cout << 0 << " ";
             }
             else cout << i << " ";
         } 
