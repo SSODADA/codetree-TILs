572a6f11c4fe0d8d051d6f0f67f6413bf9703682
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-29 17:03:04 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+ 
+    int cnt = 0;
+    
+    for(int i =1; i<=11; i++) {
+        int a;
+        cin >> a;
+        if(a%2!=0) {
+            cnt ++;
+        }
+    }
+    cout << cnt;
+
+
+
+    return 0;
+}
