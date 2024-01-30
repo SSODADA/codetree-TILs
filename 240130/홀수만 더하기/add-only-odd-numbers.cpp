79357c323fcc4628ea8ac7a446cb4c309c1391cd
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-30 15:48:50 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240130/홀수만 더하기/add-only-odd-numbers.cpp
+++ 240130/홀수만 더하기/add-only-odd-numbers.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int sum = 0;
+    // 변수 선언
+    int n, sum;
+    sum = 0;
 
-    for (int i = 1; i<=6; i++) {
+	// 입력
+	cin >> n;
+    
+	for(int i = 1; i <= n; i++) {
         int a;
         cin >> a;
-
-        if(a%2!=0 && a%3==0) {
+        if(a % 2 == 1 && a % 3 == 0)
             sum += a;
-        }
-    }
+	}
+
+    // 출력
     cout << sum;
+
     return 0;
 }
