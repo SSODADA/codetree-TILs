7890ed04c708c6ad8fd7868d7cd33aa1a013cc01
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-31 13:54:25 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240131/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -5,10 +5,20 @@ int main() {
     int a, b, sum;
     sum = 0;
     cin >> a >> b;
+
+    if(a<=b) {
     for(int i = a; i<=b; i++) {
         if(i%5==0) {
             sum += i;
         }
+    }
+    }
+    else if(a>=b) {
+        for(int i = b; i<=a; i++) {
+        if(i%5==0) {
+            sum += i;
+        }
+    }
     }
     cout << sum;
     return 0;
