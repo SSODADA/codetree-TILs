9a1596884c2fb082baf604acfde91d36a0729a6a
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-01-31 17:20:30 +0900

    Title: 주어진 수까지의 곱, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/주어진 수까지의 곱/multiplication-up-to-a-given-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, mul;
+    mul = 1;
+    cin >> a >> b;
+    for(int i = a; i<=b; i++) {
+        mul *= i;
+    }
+    cout << mul;
+    return 0;
+}
