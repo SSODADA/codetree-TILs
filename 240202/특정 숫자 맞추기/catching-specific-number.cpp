641c2f141530e79c6514375ae0cacd3300af6ebb
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-02-02 15:19:45 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240202/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    while(true) {
+        cin >> n;
+        if(n<25) {
+            cout << "Higher" << endl;
+        } 
+        else if(n>25) {
+            cout << "Lower" << endl;
+        }
+        else if(n==25) {
+            cout << "Good";
+            break;
+        }
+    }
+    
+    return 0;
+}
